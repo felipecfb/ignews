ef21bbe706cf117de5af0c3adcb808268cfcce1c
Felipe Bastos | DT3 <felipe.bastos@dt3.com.br>
2022-08-03 11:39:29 -0300

    :sparkles: feat: testando o componente signInButton

---
+++ src/components/SignInButton/SignInButton.spec.tsx
@@ -0,0 +1,41 @@
+import { render, screen } from "@testing-library/react";
+import { useSession } from "next-auth/react";
+import { SignInButton } from ".";
+
+jest.mock("next-auth/react");
+
+describe("SignInButton component", () => {
+  it("renders correctly when user is not authenticated", () => {
+    const useSessionMocked = jest.mocked(useSession);
+
+    useSessionMocked.mockReturnValueOnce({
+      data: null,
+      status: "loading",
+    });
+
+    render(<SignInButton />);
+
+    expect(screen.getByText("Sign in With Github")).toBeInTheDocument();
+  });
+});
+
+describe("SignInButton component", () => {
+  it("renders correctly when user is authenticated", () => {
+    const useSessionMocked = jest.mocked(useSession);
+
+    useSessionMocked.mockReturnValueOnce({
+      data: {
+        user: {
+          name: "John Doe",
+          email: "john.doe@example.com",
+        },
+        expires: "fake-expires",
+      },
+      status: "authenticated",
+    } as any);
+
+    render(<SignInButton />);
+
+    expect(screen.getByText(/john doe/i)).toBeInTheDocument();
+  });
+});
