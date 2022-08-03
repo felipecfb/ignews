c2f229abf88cf7c718c2089b48fe45528a6c3d56
Felipe Bastos | DT3 <felipe.bastos@dt3.com.br>
2022-08-03 11:06:37 -0300

    :sparkles: feat: testando o componente Header

--- src/components/ActiveLink/ActiveLink.spec.tsx
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { ActiveLink } from ".";
 
 jest.mock("next/router", () => {
@@ -13,13 +13,13 @@ jest.mock("next/router", () => {
 
 describe("ActiveLink component", () => {
   it("renders correctly", () => {
-    const { getByText } = render(
+    render(
       <ActiveLink href="/" activeClassName="active">
         <a href="">Home</a>
       </ActiveLink>
     );
 
-    expect(getByText("Home")).toBeInTheDocument();
+    expect(screen.getByText("Home")).toBeInTheDocument();
   });
 
   it("adds active class if the link as currenctly active", () => {
