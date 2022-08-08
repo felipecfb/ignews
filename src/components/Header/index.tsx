5dc49217f34550911e9140fcdbfd218e94fd326f
Felipe Bastos | DT3 <felipe.bastos@dt3.com.br>
2022-08-08 10:08:49 -0300

    :sparkles: feat: testing playground

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,7 @@ export function Header() {
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
-                <Image src="/images/logo.svg" alt="" width="100" height="100" />
+                <Image src="/images/logo.svg" alt="ig.news" width="100" height="100" />
                 <nav>
                     <ActiveLink activeClassName={styles.active} href="/">
                         <a className={styles.active}>Home</a>
